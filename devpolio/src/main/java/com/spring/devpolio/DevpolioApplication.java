f8f58aea7e6f0ad6f5f2b396943e12045ad563a7
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2025-05-19 11:55:39 +0900

    init: initialize this project

---
+++ devpolio/src/main/java/com/spring/devpolio/DevpolioApplication.java
@@ -0,0 +1,13 @@
+package com.spring.devpolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevpolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevpolioApplication.class, args);
+    }
+
+}
---
+++ devpolio/src/test/java/com/spring/devpolio/DevpolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.devpolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevpolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
