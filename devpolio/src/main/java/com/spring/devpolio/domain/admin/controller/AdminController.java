967483b8fbff56928c91aa7c2d9e84c4e1f76651
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2025-06-17 14:05:52 +0900

    add: swaggerapi description

--- devpolio/src/main/java/com/spring/devpolio/domain/admin/controller/AdminController.java
+++ devpolio/src/main/java/com/spring/devpolio/domain/admin/controller/AdminController.java
@@ -4,6 +4,7 @@ import com.spring.devpolio.domain.admin.dto.UserInfoResponse;
 import com.spring.devpolio.domain.user.entity.User;
 import com.spring.devpolio.domain.user.repository.UserRepository;
 import com.spring.devpolio.domain.user.service.UserService;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,18 +19,25 @@ public class AdminController {
     private final UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<UserInfoResponse>> getAllUsers() {
+    public ResponseEntity<List<UserInfoResponse>> getAllUsers(
+            @Parameter(description = "전체 사용자 목록 조회")
+            @MatrixVariable(required = false) String e
+    ) {
         return ResponseEntity.ok(userService.getAllUsersInfo());
     }
 
-    @GetMapping("/id/{userId}")
-    public ResponseEntity<UserInfoResponse> getUserById(@PathVariable Long userId) {
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserInfoResponse> getUserById(
+            @Parameter(description = "id로 특정 사용자 조회")
+            @PathVariable Long userId
+    ) {
         UserInfoResponse user = userService.getUserInfoById(userId);
         return ResponseEntity.ok(user);
     }
 
     @GetMapping("/search")
     public ResponseEntity<List<UserInfoResponse>> searchUsers(
+            @Parameter(description = "이메일 또는 사용자 이름으로 사용자 조회")
             @MatrixVariable(required = false) String name,
             @MatrixVariable(required = false) String email
     ) {
